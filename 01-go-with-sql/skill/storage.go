1e85bc45947d4ec331a4919a9bbe829c76ebaa59
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-05 20:40:45 +0700

    [01-go-with-sql/steps][extract storage] paste clipboard into storage.go file

--- 01-go-with-sql/skill/storage.go
+++ 01-go-with-sql/skill/storage.go
@@ -1,1 +1,53 @@
 package skill
+
+import (
+	"database/sql"
+	"encoding/json"
+
+	"github.com/lib/pq"
+)
+
+type storage struct {
+	db *sql.DB
+}
+
+type record struct {
+	Key         string
+	Name        string
+	Description string
+	Logo        string
+	Levels      []byte
+	Tags        pq.StringArray
+}
+
+func (r record) toSkill(lvl []Level) Skill {
+	return Skill{
+		Key:         r.Key,
+		Name:        r.Name,
+		Description: r.Description,
+		Logo:        r.Logo,
+		Tags:        r.Tags,
+		Levels:      lvl,
+	}
+}
+
+func (r record) unmarshalLevels() ([]Level, error) {
+	lvl := []Level{}
+	err := json.Unmarshal(r.Levels, &lvl)
+	return lvl, err
+}
+
+func (r record) decode(row *sql.Row) (Skill, error) {
+	if err := row.Scan(&r.Key, &r.Name, &r.Description, &r.Logo, &r.Levels, &r.Tags); err != nil {
+		return Skill{}, err
+	}
+
+	lvl, err := r.unmarshalLevels()
+	return r.toSkill(lvl), err
+}
+
+func (s storage) findSkillByKey(key string) (Skill, error) {
+	row := s.db.QueryRow("SELECT key, name, description, logo, levels, tags FROM skill WHERE key = $1", key)
+	r := record{}
+	return r.decode(row)
+}
