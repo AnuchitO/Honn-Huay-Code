7e480687411866453390b4960a70b8ef9ce8a84f
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-05 19:22:22 +0700

    [01-go-with-sql/steps] comment c.JSON and return skill, nil

--- 01-go-with-sql/skill/skill.go
+++ 01-go-with-sql/skill/skill.go
@@ -53,7 +53,8 @@ func findSkillByKey(db *sql.DB, c *gin.Context, key string) (Skill, error) {
 	skill.Tags = tags
 
 	// response the skill as JSON
-	c.JSON(http.StatusOK, gin.H{"data": skill}) // should be in handler logic
+	// c.JSON(http.StatusOK, gin.H{"data": skill}) // should be in handler logic
+	return skill, nil
 }
 
 func (h handler) GetSkillByKey(c *gin.Context) {
