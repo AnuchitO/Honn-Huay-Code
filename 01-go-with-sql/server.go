4d8d037bca11ea066408f234e6358bf61398b31c
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-05 18:44:57 +0700

    [01-go-with-sql/steps] [split file] cut all handler with method into clipboard

--- 01-go-with-sql/server.go
+++ 01-go-with-sql/server.go
@@ -27,34 +27,6 @@ type Skill struct {
 	Tags        []string `json:"tags"`
 }
 
-type handler struct {
-	db *sql.DB
-}
-
-func NewHandler(db *sql.DB) handler {
-	return handler{db: db}
-}
-
-func (h handler) GetSkillByKey(c *gin.Context) {
-	key := c.Param("key")
-	row := h.db.QueryRow("SELECT key, name, description, logo, levels, tags FROM skill WHERE key = $1", key)
-
-	var skill Skill
-	var levels []byte
-	var tags pq.StringArray
-	if err := row.Scan(&skill.Key, &skill.Name, &skill.Description, &skill.Logo, &levels, &tags); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	if err := json.Unmarshal(levels, &skill.Levels); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	skill.Tags = tags
-
-	c.JSON(http.StatusOK, gin.H{"data": skill})
-}
-
 func main() {
 	r := gin.Default()
 	r.Use(gin.Logger())
