e8764b0d59fa2e665cdcf2b698665eb1e5c1b2b5
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-05 19:23:34 +0700

    [01-go-with-sql/steps] repsone skill as JSON when it success

--- 01-go-with-sql/skill/skill.go
+++ 01-go-with-sql/skill/skill.go
@@ -66,4 +66,6 @@ func (h handler) GetSkillByKey(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+
+	c.JSON(http.StatusOK, gin.H{"data": skill})
 }
