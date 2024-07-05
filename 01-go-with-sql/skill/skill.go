0ac06acab57780db8d9a2dedeca82ef44f604e6e
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-05 20:40:18 +0700

    [01-go-with-sql/steps][extract storage] cut all storage relate into clipboard

--- 01-go-with-sql/skill/skill.go
+++ 01-go-with-sql/skill/skill.go
@@ -34,51 +34,6 @@ func NewHandler(db *sql.DB) handler {
 	return handler{storage: storage{db: db}}
 }
 
-type storage struct {
-	db *sql.DB
-}
-
-type record struct {
-	Key         string
-	Name        string
-	Description string
-	Logo        string
-	Levels      []byte
-	Tags        pq.StringArray
-}
-
-func (r record) toSkill(lvl []Level) Skill {
-	return Skill{
-		Key:         r.Key,
-		Name:        r.Name,
-		Description: r.Description,
-		Logo:        r.Logo,
-		Tags:        r.Tags,
-		Levels:      lvl,
-	}
-}
-
-func (r record) unmarshalLevels() ([]Level, error) {
-	lvl := []Level{}
-	err := json.Unmarshal(r.Levels, &lvl)
-	return lvl, err
-}
-
-func (r record) decode(row *sql.Row) (Skill, error) {
-	if err := row.Scan(&r.Key, &r.Name, &r.Description, &r.Logo, &r.Levels, &r.Tags); err != nil {
-		return Skill{}, err
-	}
-
-	lvl, err := r.unmarshalLevels()
-	return r.toSkill(lvl), err
-}
-
-func (s storage) findSkillByKey(key string) (Skill, error) {
-	row := s.db.QueryRow("SELECT key, name, description, logo, levels, tags FROM skill WHERE key = $1", key)
-	r := record{}
-	return r.decode(row)
-}
-
 func (h handler) GetSkillByKey(c *gin.Context) {
 	// get the key from the URL path param
 	key := c.Param("key")
