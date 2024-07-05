af24d461040b7f30cbb6d48960f3438935acd81c
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-05 18:57:05 +0700

    [01-go-with-sql/steps][WORK-cover] [over test] add GetSkillByKey success case

--- 01-go-with-sql/skill_test.go
+++ 01-go-with-sql/skill_test.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"database/sql"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	_ "modernc.org/sqlite"
+)
+
+var table = `CREATE TABLE IF NOT EXISTS skill (
+	key TEXT PRIMARY KEY,
+	name TEXT NOT NULL DEFAULT '',
+	description TEXT NOT NULL DEFAULT '',
+	logo TEXT NOT NULL DEFAULT '',
+	levels JSONB NOT NULL DEFAULT '[]',
+	tags TEXT [] NOT NULL DEFAULT '{}'
+);`
+
+func TestSkillHandler(t *testing.T) {
+	t.Run("GetSkillByKey: should response skill json successfuly", func(t *testing.T) {
+		db, _ := sql.Open("sqlite", "file:TestSkillHandler?mode=memory&cache=shared")
+		defer db.Close()
+		gosql := `INSERT INTO skill (key, name, description, levels, tags)
+		VALUES (
+			'go', 'Go', 'Go is an open source programming...',
+			'[{"name": "Beginner", "level": 1, "descriptions": ["knowledge ..."]},{"name": "Intermediate", "level": 2, "descriptions": ["complex programs..."]}]',
+			'{go,golang}'
+		);`
+
+		db.Exec(table)
+		db.Exec(gosql)
+
+		rec := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(rec)
+		c.Params = append(c.Params, gin.Param{Key: "key", Value: "go"})
+
+		h := NewHandler(db)
+		h.GetSkillByKey(c)
+
+		want := `{"data":{"key":"go","name":"Go","description":"Go is an open source programming...","logo":"","levels":[{"key":"","name":"Beginner","brief":"","descriptions":["knowledge ..."],"level":1},{"key":"","name":"Intermediate","brief":"","descriptions":["complex programs..."],"level":2}],"tags":["go","golang"]}}`
+		got := rec.Body.String()
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
